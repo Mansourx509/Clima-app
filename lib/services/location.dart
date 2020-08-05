05b4d56ad898a4d87fb735ea8a216e2297ff4d06
Mohamed Mansour <44348521+Mansourx509@users.noreply.github.com>
2020-08-05 06:28:41 +0200

    Add files via upload

---
+++ lib/services/location.dart
@@ -0,0 +1,24 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location{
+
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator().getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    }
+    catch(exception){
+      print(exception);
+    }
+  }
+}
+/*
+double temperature = jsonDecoded['main']['temp'];
+int condition = jsonDecoded['weather'][0]['id'];
+String cityName = jsonDecoded['name'];
+ */
